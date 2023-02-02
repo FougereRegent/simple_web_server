de57c05c8d28ea3c3069a08dd558ce72fcc7fd1d
damien venant-valéry <damien.venant@outlook.com>
2023-02-02 12:13:03 +0100

    Ajout de tous les code HTTP

--- src/main.c
+++ src/main.c
@@ -20,14 +20,83 @@ typedef enum {
 } ENUM_VERB;
 
 typedef enum {
+	CONTINUE = 100,
+	SWITCH_PROTOCOLS = 101,
+	PROCESSING = 102,
+	EARLY_HINTS = 103,
 	OK = 200,
+	CREATED = 201,
+	ACCEPTED = 202,
+	NON_AUTHORITATIVE_INFORMATION = 203,
+	NO_CONTENT = 204,
+	RESET_CONTENT = 205,
+	PARTIAL_CONTENT = 206,
+	MULTI_STATUS = 207,
+	ALREADY_REPORTED = 208,
+	CONTENT_DIFFERENT = 210,
+	IM_USED = 226,
+	MULTIPLE_CHOICES = 300,
+	MOVED_PERMANENTLY = 301,
+	FOUND = 302,
+	SEE_OTHER = 303,
+	NOT_MODIFIED = 304,
+	USE_PROXY = 305,
+	SWITCH_PROXY = 306,
+	TEMPORALY_REDIRECT = 307,
+	PERMANENT_REDIRECT = 308,
+	TOO_MANY_REDIRECT = 310,
+	BAD_REQUEST = 400,
+	UNAUTHORIZED = 401,
+	PAYMENT_REQUIRED = 402,
+	FORBIDDEN = 403,
 	NOT_FOUND = 404,
+	METHOD_NOT_ALLOWED = 405,
+	NOT_ACCEPTABLE = 406,
+	PROXY_AUTHENTICATION_REQUIRED = 407,
+	REQUEST_TIME_OUT = 408,
+	CONFLICT = 409,
+	GONE = 410,
+	LENGHT_REQUIRED = 411,
+	PRECONDITION_FAILED = 412,
+	REQUEST_ENTITY_TOO_LARGE = 413,
+	REQUEST_URI_TOO_LONG = 414,
+	UNSUPPORTED_MEDIA_TYPE = 415,
+	REQUEST_RANGE_UNSATISFIABLE = 416,
+	EXCEPTATION_FAILED = 417,
+	TEAPOT = 418,
+	PAGE_EXPIRED = 419,
+	BAD_MAPPING = 421,
+	UNPROCESSABLE_ENTITY = 422,
+	LOCKED = 423,
+	METHOD_FAILURE = 424,
+	TOO_EARLY = 425,
+	UPGRADE_REQUIRED = 426,
+	PRECONDITION_REQUIRED = 428,
+	TOO_MANY_REQUEST = 429,
+	REQUEST_HEADER_FIELDS_TOO_LARGE = 431,
+	RETRY_WITH = 449,
+	BLOCKED_BY_WINDOWS_PARENTAL_CONTROL = 450,
+	UNVAILABLE_FOR_LEGAL_REASON = 451,
+	UNRECOVERABLE_ERROR = 456,
+	INTERNAL_SERVER_ERROR = 500,
+	NOT_IMPLEMENTED = 501,
+	BAD_GATEWAY = 502,
+	SERVICE_UNAVAILABLE = 503,
+	GATEWAY_TIME_OUT = 504,
+	HTTP_VERSION_NOT_SUPPORTED = 505,
+	VARIANT_ALSO_NEGOTIATES = 506,
+	INSUFFICIENT_STORAGE = 507,
+	LOOP_DETECTED = 508,
+	BANDWITH_LIMIT_EXCEEDED = 509,
+	NOT_EXTENDED = 510,
+	NETWORK_AUTHENTIFICATION_REQUIRED = 511
+
 } ENUM_CODE;
 
 
 static void handle_connection(int, struct sockaddr_in*);
 static void wich_verb(int, char*, ENUM_VERB);
-static void send_header(int, int);
+static void send_header(int, ENUM_CODE);
 static void get_verb(int, const char*);
 static void put_verb(int, const char*);
 static void delete_verb(int, const char*);
@@ -147,9 +216,17 @@ static void wich_verb(int sock, char *route,ENUM_VERB verb)
 	}
 }
 
-static void send_header(int sock, int error_code) 
+static void send_header(int sock, ENUM_CODE error_code) 
 {
-	const char *header = "HTTP/1.1 200 OK\r\nServer: tiny-server\r\n\r\n";
+	const char *header = "Server: tiny-server\r\n\r\n"
+						"Author: Venant-Valéry Damien";
+
+	switch(error_code) {
+		case NOT_FOUND:
+			break;
+		case OK:
+			break;
+	}
 	send_message(sock, (unsigned char*)header);
 }
 
