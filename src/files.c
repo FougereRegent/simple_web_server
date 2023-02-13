590a98b872853fe7e8841da6f0c1ef311408fdaa
damien venant-valéry <damien.venant@outlook.com>
2023-02-13 21:55:09 +0100

    Ajout de la lecture du fichier

--- src/files.c
+++ src/files.c
@@ -10,9 +10,15 @@
 
 
 static int check_file(const char *filename);
+static char* reel_path(const char* filename);
 
 extern struct file_info *open_file(const char *filename) {
-	return NULL;
+	struct file_info *info = (struct file_info*) malloc(sizeof(struct file_info));
+	info->filename = reel_path(filename);
+	info->size = file_size(filename);
+	if((info->fp = fopen(info->filename, "r")) == NULL)
+		return NULL;
+	return info;
 }
 
 extern char *file_content(const FILE *fp) {
@@ -21,10 +27,8 @@ extern char *file_content(const FILE *fp) {
 
 extern long file_size(const char *filename) {
 	long file_size = 0;
-	char pathfile[255] = {'\0'};
+	char *pathfile =  reel_path(filename);
 	struct stat st;
-
-	sprintf(pathfile, "%s%s", FILE_PATH, filename);
 
 	if(check_file(pathfile) == -1)
 		return NON_AUTHORISATION;
@@ -32,9 +36,17 @@ extern long file_size(const char *filename) {
 	if(stat(filename, &st) == -1)
 		return -1;
 	file_size = st.st_size;
+	free(pathfile);
 	return file_size;
 }
 
 static int check_file(const char *filename) {
 	return access(filename, F_OK | R_OK);
+}
+
+static char* reel_path(const char* filename) {
+	char *pathfile =  (char*)malloc(sizeof(char) * 255);
+	memset((char*)pathfile, 0, 255);
+	sprintf(pathfile, "%s/%s", FILE_PATH, filename);
+	return pathfile;
 }
