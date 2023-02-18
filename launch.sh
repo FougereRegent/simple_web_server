fca3934ac54ef999d64da864aeaee21cca7611e4
FouegereRegent <damien.venant@outlook.com>
2023-02-18 14:49:27 +0100

    Ajout d'un dockerfile pour le debug de l'application Modification du script pour lancer les conteneurs

--- launch.sh
+++ launch.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+nb_args=$#
+args=$1
 
-docker build -t server_web .
-docker run -it -p 8080:8080 server_web
+function startDocker() {
+	docker build -t server_web .
+	docker run -it -p 8080:8080 server_web
+}
+
+if [ $nb_args -ne 1 ]; then
+	startDocker
+else
+	if [ "$args" == "debug" ]; then
+		echo "Hey"
+		docker build -f DockerfileGdb -t server_web:debug .
+		docker run -it -p 8080:8080 server_web:debug
+	else
+		startDocker
+	fi
+fi
